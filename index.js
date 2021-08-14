40f89a333f13af204e989427e1e63d1af27072aa
ombralunare <83118308+ombralunare@users.noreply.github.com>
2021-08-14 07:55:12 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+//
+let "button" = document.createElementById("shop")
+let "" = document.createElementById("vado")
+let "" = document.createElementById("nota")
+let "" = document.createElementById("heed")
+
+
+let "SHOPPING" = document.getElementById("shop")
+let "Travel" = document.getElementById("vado")
+let "Notes to Self" = document.getElementById("nota")
+let "Reminders" = document.getElementById("heed") //
